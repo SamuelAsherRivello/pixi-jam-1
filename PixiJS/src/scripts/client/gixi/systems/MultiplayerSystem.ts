8c8abe19dee80647d2d16f1dee828151655f4456
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-07-24 16:15:54 -0500

    Client and Server Bi-directional communication working

--- PixiJS/src/scripts/client/gixi/systems/MultiplayerSystem.ts
+++ PixiJS/src/scripts/client/gixi/systems/MultiplayerSystem.ts
@@ -29,17 +29,40 @@ export class MultiplayerSystem extends SystemBase implements ISystemBase {
 
         this.socket = io('http://localhost:3001');
 
-        // this.socket.on('connect', () => {
-        //     this.consoleLog('connected to server');
+        this.socket.on('connect', () => {
+            this.consoleLog('client connected to server 123');
 
-        //     const request = new SessionStartRequest();
-        //     this.socket.emit(SessionStartRequest.name, request.toJSON());
-        // });
+            const request = new SessionStartRequest();
+            this.socket.emit(SessionStartRequest.name, JSON.stringify(request));
+        });
 
-        // this.socket.on(SessionStartResponse.name, (data: string) => {
-        //     const response = Packet.fromJSON(data, SessionStartResponse);
-        //     this.consoleLog(`Received response: ${JSON.stringify(response)}`);
-        // });
+        this.socket.on(SessionStartResponse.name, (data) => {
+            console.log('Received data 5 :', data);
+            const response = Packet.fromJSON(data, SessionStartResponse);
+            console.log('Received packet 6 :', response);
+
+            const request = new GameCreateRequest();
+            this.socket.emit(GameCreateRequest.name, JSON.stringify(request));
+
+        });
+
+        this.socket.on(GameCreateResponse.name, (data) => {
+            console.log('Received data 9 :', data);
+            const response = Packet.fromJSON(data, GameCreateResponse);
+            console.log('Received packet 10 :', response);
+
+
+            const request = new GameJoinRequest();
+            this.socket.emit(GameJoinRequest.name, JSON.stringify(request));
+
+        });
+
+        this.socket.on(GameJoinResponse.name, (data) => {
+            console.log('Received data 99 :', data);
+            const request = Packet.fromJSON(data, GameJoinResponse);
+            console.log('Received packet 1010 :', request);
+
+        });
 
         this.socket.on('disconnect', () => {
             this.consoleLog('disconnected from server');
@@ -53,5 +76,75 @@ export class MultiplayerSystem extends SystemBase implements ISystemBase {
             return;
         }
         console.log(`[${(MultiplayerSystem).name}] ${msg}`);
+    }
+}
+
+
+
+
+
+
+
+class Packet {
+
+    public get name(): string {
+        return this.constructor.name;
+    }
+    constructor() {
+
+        console.log('Packet constructor:', this.name);
+    }
+
+    static fromJSON<T extends Packet>(json: string, cls: new (...args: any[]) => T): T {
+        const obj = typeof json === "string" ? JSON.parse(json) : json;
+        return new cls(obj.name);
+    }
+}
+
+class Request extends Packet {
+    constructor() {
+        super();
+    }
+}
+
+class Response extends Packet {
+    constructor() {
+        super();
+    }
+}
+
+class SessionStartRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class SessionStartResponse extends Response {
+    constructor() {
+        super();
+    }
+}
+
+class GameCreateRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class GameCreateResponse extends Response {
+    constructor() {
+        super();
+    }
+}
+
+class GameJoinRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class GameJoinResponse extends Response {
+    constructor() {
+        super();
     }
 }
--- PixiJS/src/scripts/server/server.ts
+++ PixiJS/src/scripts/server/server.ts
@@ -1,20 +1,120 @@
 import { Server } from "socket.io";
-//import { Packet, SessionStartRequest, SessionStartResponse } from "@shared/Packet";
+import http from "http";
+import { json } from "stream/consumers";
 
-const io = new Server(3001);
+const server = http.createServer();
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Adjust this to your client's URL if needed
+        methods: ["GET", "POST"]
+    }
+});
+
+server.listen(3001, () => {
+    console.log('Server started on port 3001');
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected2');
+    socket.on(SessionStartRequest.name, (data) => {
+        console.log('Received data 1 :', data);
+        const request = Packet.fromJSON(data, SessionStartRequest);
+        console.log('Received packet 2 :', request);
 
-    // socket.on(SessionStartRequest.name, (data: string) => {
-    //     const packet = Packet.fromJSON(data, SessionStartRequest);
-    //     console.log('Received packet:', packet);
+        const response = new SessionStartResponse();
+        socket.emit(SessionStartResponse.name, JSON.stringify(response));
+    });
 
-    //     const response = new SessionStartResponse();
-    //     socket.emit(SessionStartResponse.name, response.toJSON());
-    // });
+
+    socket.on(GameCreateRequest.name, (data) => {
+        console.log('Received data A :', data);
+        const request = Packet.fromJSON(data, GameCreateRequest);
+        console.log('Received packet B :', request);
+
+        const response = new GameCreateResponse();
+        socket.emit(GameCreateResponse.name, JSON.stringify(response));
+
+    });
+
+    socket.on(GameJoinRequest.name, (data) => {
+        console.log('Received data aaa :', data);
+        const request = Packet.fromJSON(data, GameJoinRequest);
+        console.log('Received packet bbb', request);
+
+        const response = new GameJoinResponse();
+        socket.emit(GameJoinResponse.name, JSON.stringify(response));
+
+    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
+
+
+
+
+
+class Packet {
+
+    public get name(): string {
+        return this.constructor.name;
+    }
+    constructor() {
+
+        console.log('Packet constructor:', this.name);
+    }
+
+    static fromJSON<T extends Packet>(json: string, cls: new (...args: any[]) => T): T {
+        const obj = typeof json === "string" ? JSON.parse(json) : json;
+        return new cls(obj.name);
+    }
+}
+
+class Request extends Packet {
+    constructor() {
+        super();
+    }
+}
+
+class Response extends Packet {
+    constructor() {
+        super();
+    }
+}
+
+class SessionStartRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class SessionStartResponse extends Response {
+    constructor() {
+        super();
+    }
+}
+
+class GameCreateRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class GameCreateResponse extends Response {
+    constructor() {
+        super();
+    }
+}
+
+class GameJoinRequest extends Request {
+    constructor() {
+        super();
+    }
+}
+
+class GameJoinResponse extends Response {
+    constructor() {
+        super();
+    }
+}
